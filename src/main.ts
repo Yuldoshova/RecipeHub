0944d0de935659b3ba01c52394abee47167bb3cd
Yuldoshova <ozoda8171@gmail.com>
2024-11-24 22:37:45 +0500

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = app.get(ConfigService)
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('RecipeHub')
+    .setDescription('The RecipeHub API description')
+    .setVersion('1.0')
+    .addTag('apis')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  const appConfig = app.get(ConfigService)
 
   await app.listen(
-    config.get<number>('appConfig.port'),
-    config.get<string>('appConfig.host'), () => {
-      console.log(`Server running port on ${config.get<number>('appConfig.port')}`)
+    appConfig.get<number>('appConfig.port'),
+    appConfig.get<string>('appConfig.host'), () => {
+      console.log(`Server running port on ${appConfig.get<number>('appConfig.port')}`)
     });
 }
 bootstrap();
