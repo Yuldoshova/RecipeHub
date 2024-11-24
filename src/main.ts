3c2474e8fead4ee16ed2fc83500542e00597dd59
Yuldoshova <ozoda8171@gmail.com>
2024-11-24 23:51:31 +0500

    added validation

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({transform:true}));
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('RecipeHub')
--- src/modules/category/dto/create-category.dto.ts
+++ src/modules/category/dto/create-category.dto.ts
@@ -1,1 +1,16 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty, IsOptional, IsString } from "class-validator"
+
+export class CreateCategoryDto {
+
+    @IsString()
+    @IsNotEmpty()
+    name:string
+
+    @IsString()
+    @IsOptional()
+    description:string
+
+    @IsString()
+    @IsOptional()
+    imageUrl:string
+}
