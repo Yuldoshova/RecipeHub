03aab2581f0135d3882428c8cec845937cae074f
Yuldoshova <ozoda8171@gmail.com>
2024-11-24 23:18:30 +0500

    added typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { appConfig } from './config/app.config';
 import { dbConfig } from './config/database.config';
+import { Category } from './modules/category/entities/category.entity';
 import { CategoryModule } from './modules/category/category.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [appConfig, dbConfig]
+      load: [appConfig, dbConfig],
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        port: configService.get<number>('dbConfig.port'),
+        host: configService.get<string>('dbConfig.host'),
+        username: configService.get<string>('dbConfig.user'),
+        password: configService.get<string>('dbConfig.password'),
+        database: configService.get<string>('dbConfig.dbName'),
+        entites: [Category],
+        synchronize: true,
+      }),
+      inject: [ConfigService]
     }),
     CategoryModule
   ],
--- src/modules/category/entities/category.entity.ts
+++ src/modules/category/entities/category.entity.ts
@@ -1,1 +1,17 @@
-export class Category {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: "categories" })
+export class Category {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({ name: "name", type: "varchar", unique: true, length: 100, nullable: false })
+    name: string
+
+    @Column({ name: "description", type: "text", length: 500, nullable: true })
+    description: string
+
+    @Column({ name: "image_url", type: "varchar", nullable: true })
+    imageUrl: string
+}
